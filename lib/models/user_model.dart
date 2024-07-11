2206af89fcb835afe81ee4a0fe316dde481f6d89
Eyob <eyobketema8@gmail.com>
2024-07-12 00:16:42 +0300

    Refactor User class to UserModel and add toMap() and fromMap() methods

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,21 +1,30 @@
-class User {
-  String? id;
+class UserModel {
+  String? uid;
   String? contact;
+  String? email;
   String? username;
   String? updatedAt;
 
-  Map<String, dynamic>? details;
-
-  User({
-    this.id,
+  UserModel({
+    this.uid,
     this.contact,
+    this.email,
     this.username,
   });
+  Map<String, dynamic> toMap() {
+    return {
+      'uid': uid,
+      'email': email,
+      'contact': contact,
+      'username': username,
+    };
+  }
 
-  factory User.fromJson(Map<String, dynamic> value) {
-    return User(
-      id: value["id"],
+  factory UserModel.fromMap(Map<String, dynamic> value) {
+    return UserModel(
+      uid: value["uid"],
       contact: value["contact"],
+      email: value["email"],
       username: value['username'],
     );
   }
