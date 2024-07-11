ecd2ad398af5e2e802b84c809a387086a71d5eb7
Eyob <eyobketema8@gmail.com>
2024-07-12 00:16:11 +0300

    Add Firebase initialization and navigation routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,36 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:rental_management/firebase_options.dart';
+import 'package:rental_management/providers/authentication_provider.dart';
+import 'package:rental_management/screens/landing_screen.dart';
+import 'package:rental_management/screens/login_screen.dart';
 import 'package:rental_management/screens/property_listing.dart';
+import 'package:rental_management/screens/property_post.dart';
+import 'package:rental_management/screens/signup_screen.dart';
+import 'package:rental_management/utils/colors.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => AuthenticationProvider(),
+      child: MaterialApp(
+        theme: CustomTheme.themeData,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => LandingScreen(),
+          '/login': (context) => LoginScreen(),
+          '/signup': (context) => SignupScreen(),
+          '/property_listing': (context) => const PropertyListing(),
+          '/propertyposting': (context) => const PropertyPost(),
+        },
+      ),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -11,51 +39,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Rental Management App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
         useMaterial3: true,
       ),
-      home: const PropertyListing(),
+      home: LandingScreen(),
       //home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  final String title;
-
-  const MyHomePage({super.key, required this.title});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final int _counter = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
